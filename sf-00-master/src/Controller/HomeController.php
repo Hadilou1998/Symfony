171c372abe4dd2be133e85c4dd6cfa62c4050f43
Hadilou1998 <hadilissilame@gmail.com>
2024-09-09 09:43:36 +0200

    Updated HomeController.php and index.html.twig

--- sf-00-master/src/Controller/HomeController.php
+++ sf-00-master/src/Controller/HomeController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\PersonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/{name}', name: 'home', methods: ['GET'])] // Ceci est une annotation PHP, ici elle permet de définir une route pour le contrôleur et ses paramètres.
-    public function index(): Response // Ceci est le contrôleur qui va être appelé lorsque l'utilisateur accède à la page '/'
+    #[Route('/', name: 'home', methods: ['GET'])] // Ceci est une annotation PHP, ici elle permet de définir une route pour le contrôleur et ses paramètres.
+    public function index(PersonRepository $pr): Response // Ceci est le contrôleur qui va être appelé lorsque l'utilisateur accède à la page '/'
     {
-        $name = ''; // Nous allons l'envoyer à la vue.
-        return $this->render('home/index.html.twig', [ // La méthode render() de la classe AbstractController permet de renvoyer un template avec des données.
+        $name = 'Alex'; // À l'aide de la classe Request de HttpFoundation, on peut récupérer les paramètres de la route.
+        return $this->render('setting/index.html.twig', [ // La méthode render() de la classe AbstractController permet de renvoyer un template avec des données.
             'name' => $name ?? 'Nobody', // Si le paramètre n'est pas renseigné, on lui donne un nom par défaut.
+            'people' => $pr->findAll(), // On récupère tous les personnages dans la base de données.
         ]);
     }
 }
