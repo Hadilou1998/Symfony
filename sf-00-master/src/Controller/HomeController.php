5c4290d868e299ef0b4a1cf8359c43fd5764e5dd
Hadilou1998 <hadilissilame@gmail.com>
2024-09-09 09:36:12 +0200

    Updated HomeController.php

--- sf-00-master/src/Controller/HomeController.php
+++ sf-00-master/src/Controller/HomeController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/{name}', name: 'home', methods: ['GET'])] // Ceci est une annotation PHP, ici elle permet de définir une route pour le contrôleur et ses paramètres.
+    public function index(): Response // Ceci est le contrôleur qui va être appelé lorsque l'utilisateur accède à la page '/'
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $name = ''; // Nous allons l'envoyer à la vue.
+        return $this->render('home/index.html.twig', [ // La méthode render() de la classe AbstractController permet de renvoyer un template avec des données.
+            'name' => $name ?? 'Nobody', // Si le paramètre n'est pas renseigné, on lui donne un nom par défaut.
         ]);
     }
 }
